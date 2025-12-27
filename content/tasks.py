ffa4c1b16d7e82620a1ce4fbac24a060076eac34
VincentGörner <161941709+A1exi0sD4rc@users.noreply.github.com>
2025-12-27 16:53:00 +0100

    update content-App, signals.py, tasks.py, admin.py

--- content/tasks.py
+++ content/tasks.py
@@ -1,6 +1,6 @@
 import subprocess
 
 def convert_480p(source):
-    target = source + '_480p.mp4'
-    cmd = 'ffmpeg -i "{}" -s hd480 -c:v libx264 -crf 23 -c:a aac -strict -2 "{}"'.format(source, target)
-    subprocess.run(cmd)
+    new_file = source + '_480p.mp4'
+    cmd = 'ffmpeg -i "{}" -s hd480 -c:v libx264 -crf 23 -c:a aac -strict -2 "{}"'.format(source, new_file)
+    subprocess.run(cmd, capture_output=True)
