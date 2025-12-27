c0cf57a8ed164882efb8377bbc8bc760705e530c
VincentGörner <161941709+A1exi0sD4rc@users.noreply.github.com>
2025-12-27 16:17:36 +0100

    update content App / models and signals and add tasks.py

--- content/models.py
+++ content/models.py
@@ -1,3 +1,11 @@
 from django.db import models
+from datetime import date
 
-# Create your models here.
+class Video(models.Model):
+    created_at = models.DateField(default=date.today)
+    title = models.CharField(max_length=255)
+    description = models.CharField(max_length=255)
+    video_file = models.FileField(upload_to='video', blank=True, null=True)
+
+    def __str__(self):
+        return self.title
--- content/signals.py
+++ content/signals.py
@@ -1,11 +1,20 @@
-from .models import Video
+import os
 from django.dispatch import receiver
-from django.db.models.signals import post_save
+from django.db.models.signals import post_save, post_delete
+from .models import Video
+from .tasks import convert_480p
 
-@receiver (post_save, sender=Video)
+@receiver (post_save, sender=Video) # Variante 2 zur Verbindung
 def video_post_save(sender, instance, created, **kwargs):
     print('Video wurde gespeichert')
     if created:
         print('New Video created')
+        convert_480p(instance.video_file.path)
 
 # post_save.connect(video_post_save, sender=Video) # Variante 1 zur Verbindung 
+
+@receiver(post_delete, sender=Video)
+def auto_delete_file_on_delete(sender, instance, **kwargs):
+    if instance.file:
+        if os.path.isfile(instance.video_file.path):
+            os.remove(instance.video_file.path)
